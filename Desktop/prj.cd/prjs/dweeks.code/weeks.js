657666f327fb48527bef2881d5d222bceb45c973
Faraj <omarfaraj355@gmail.com>
2024-01-16 14:02:38 -0500

    a simplr days of the weeks generator, by javascript

---
+++ Desktop/prj.cd/prjs/dweeks.code/weeks.js
@@ -0,0 +1,7 @@
+// Create an array with the names of the days
+const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+// Print each day of the week using a loop
+for (let i = 0; i < daysOfWeek.length; i++) {
+  console.log(daysOfWeek[i]);
+}
